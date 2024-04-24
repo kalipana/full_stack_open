853fc282e871ca1188db533190748e1db58cc993
Kian Alipanah <137922153+kalipana@users.noreply.github.com>
2024-04-24 17:23:44 -0400

    anecdotes final

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+
+  const handleNew = () => {
+    const newSelected = Math.floor(Math.random() * anecdotes.length)
+    setSelected(newSelected)
+    console.log(newSelected)
+  }
+
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    console.log(copy)
+    setVotes(copy)
+  }
+
+  const maxIndex = (arr) => {
+    if (arr.length == 0) {
+      return -1
+    }
+
+    var max = arr[0]
+    var maxIndex = 0;
+
+    for (var i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {
+        maxIndex = i
+        max = arr[i]
+      }
+    }
+    return maxIndex
+  }
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+
+  const points = new Uint16Array(anecdotes.length)
+  const [votes, setVotes] = useState(points)
+
+  const maxVotes = maxIndex(votes)
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <br></br>
+      has {votes[selected]} votes.
+      <br></br>
+      <Button handleClick={handleVote} text="vote" />
+      <Button handleClick={handleNew} text="next anecdote" />
+      <br></br>
+      <h1>Anecdote with the most votes</h1>
+      <div>
+        {anecdotes[maxVotes]}
+        <br></br>
+        has {votes[maxVotes]} votes.
+      </div>
+    </div>
+  )
+}
+
+export default App
